c14e0403a70da33b528a27ffe6d59c52b652daff
gabriellgrehov <eugenekalnoy@gmail.com>
2023-03-11 19:49:53 -0800

    Demo 2.0 start

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
+import { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import Bed220 from './assets/iconsSVG/bed_220';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'LilitaOne': require('./assets/fonts/LilitaOne.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontFamily: 'LilitaOne', fontSize: 30 }}>LilitaOne</Text>
+      <Text style={{ fontSize: 30 }}>Platform Default</Text>
+      <Bed220 />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -13,7 +35,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F0CE6A',
     alignItems: 'center',
     justifyContent: 'center',
   },
